e18d4abc92ee29cb010d88e09156b8e677c4c763
Darius Feher <darius.feher@student.manchester.ac.uk>
2021-10-27 10:28:37 +0100

    Add website icon

--- visionStudios/urls.py
+++ visionStudios/urls.py
@@ -17,6 +17,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 from . import views
 
@@ -28,7 +30,11 @@ urlpatterns = [
     path('galerie', views.galeryPage, name="galeryPage"),
     path('contact', views.contactPage, name="contactPage"),
     path('submitFloater', views.submitFloater, name="submitFloater"),
-    path('aplica', views.applyPage, name="applyPage")
+    path('aplica', views.applyPage, name="applyPage"),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
